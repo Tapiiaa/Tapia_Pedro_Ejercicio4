bc6ace3c1161269ca16173a91dcec44706a12f1c
Tapiiaa <ptapilob@myuax.com>
2024-02-15 23:40:32 +0100

    Ejercicio 7

--- src/Ejercicio7/Aplicacion.java
+++ src/Ejercicio7/Aplicacion.java
@@ -1,4 +1,17 @@
 package Ejercicio7;
-
+import java.util.Scanner;
 public class Aplicacion {
+    public static void main(String[]args){
+        GeneradorDeCitas generador = new GeneradorDeCitas();
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Cita inspiradora del día: "+generador.obtenerCita());
+            System.out.println("Teclee salir");
+            String salida = sc.nextLine();
+            if(salida.equals("salir")){
+                break;
+            }
+        }
+        sc.close();
+    }
 }
